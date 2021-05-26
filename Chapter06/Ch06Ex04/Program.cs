30edd5503c202b2339eb3779fd141b1acfd1cba6
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-05-26 19:06:02 -0400

    Chapter06

---
+++ Chapter06/Ch06Ex04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ch06Ex04
+{
+    class Program
+    {
+        static void Write()
+        {
+            string myString = "String defined in Write()";
+            Console.WriteLine("Now in Write()");
+            Console.WriteLine("myString = {0}", myString);
+        }
+        static void Main(string[] args)
+        {
+            string myString = "String defined in Main()";
+            Write();
+            Console.WriteLine("\nNow in Main()");
+            Console.WriteLine("myString = {0}", myString);
+            Console.ReadKey();
+        }
+    }
+}
